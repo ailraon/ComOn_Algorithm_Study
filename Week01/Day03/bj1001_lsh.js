c60b379454287e84337f85e1859fdca7b54f765c
Cat Hanbit <72328552+LeeSeunghyeon-1@users.noreply.github.com>
2021-06-24 00:17:59 +0900

    Create bj1001_lsh.js

---
+++ Week01/Day03/bj1001_lsh.js
@@ -0,0 +1,16 @@
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const numbers = input().split(" ");
+
+const a = parseInt(numbers[0]);
+const b = parseInt(numbers[1]);
+
+console.log(a - b);
