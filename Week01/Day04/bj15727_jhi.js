04f0ddd40f4c2f3fc17902e00565f3cec3a00763
ailraon <aadsf1789@naver.com>
2021-06-24 01:29:01 +0900

    Day04 문제 풀이

---
+++ Week01/Day04/bj15727_jhi.js
@@ -0,0 +1,19 @@
+// 한글 입력 잘 받게 하는 코드
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+// input() 할 때마다 한 줄이 문자열로 입력받아짐
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const len = input();
+
+let time = parseInt(len/5);
+if(parseInt(len)%5 > 0) {
+    time++;
+}
+console.log(time);
