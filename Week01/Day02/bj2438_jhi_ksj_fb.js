37b47bc19d5daca7ef405ced0582be15a6ac296d
ailraon <aadsf1789@naver.com>
2021-06-24 01:40:04 +0900

    Day02 피드백 답안 작성 완료

---
+++ Week01/Day02/bj2438_jhi_ksj_fb.js
@@ -0,0 +1,46 @@
+// 한글 입력 잘 받게 하는 코드
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+// input() 할 때마다 한 줄이 문자열로 입력받아짐
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let number = parseInt(input());
+
+// 2438 번 문제
+for (let i = 0; i < number; i++) {
+  //// 문자열 + 연산 이용한거 참신하네요
+  //// "*".repeat(i)를 이용했으면 더 깔끔했을 것 같아요
+
+  // └ 확실히 더 깔끔해 진것 같습니다. 
+
+  //// 연산자 + 를 이용한 점 좋았던 것 같아요
+  //// 잘한거 같습니다
+
+  // └ 칭찬 감사합니다!
+  
+  let msg = "*".repeat(i+1);
+  
+  console.log(msg);
+}
+
+// 2439 번 문제
+for (let i = 0; i < number; i++) {
+  let msg = "";
+  msg += " ".repeat(number-i-1);
+  msg += "*".repeat(i+1);
+  console.log(msg);
+}
+
+// 2442 번 문제
+for (let i = 0; i < number; i++) {
+  let msg = "";
+  msg += " ".repeat(number-i-1);
+  msg += "*".repeat(i*2+1);
+  console.log(msg);
+}
