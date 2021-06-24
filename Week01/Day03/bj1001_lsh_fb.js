7e69d48cc07a8f8c57b7c7625138e44251389727
Cat Hanbit <72328552+LeeSeunghyeon-1@users.noreply.github.com>
2021-06-24 14:43:20 +0900

    .

---
+++ Week01/Day03/bj1001_lsh_fb.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const numbers = input().split(" ");
+
+const a = parseInt(numbers[0]);
+const b = parseInt(numbers[1]);
+
+console.log(a - b);
