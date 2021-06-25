d62e11a27af56fc3e93dc8672066baa8dc3ce758
ailraon <aadsf1789@naver.com>
2021-06-25 15:55:19 +0900

    Day05 문제 풀이 완료

---
+++ Week01/Day05/bj8393_jhi.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let num = parseInt(input());
+console.log((num/2+0.5) * num);
+//입력값   : 1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  10
+//출력값   : 1  |  3  |  6  | 10  | 15  | 21  | 28  | 36  | 45  |  55
+//증가비율 : 1  | 1.5 |  2  | 2.5 |  3  | 3.5 |  4  | 4.5 |  5  |  5.5
