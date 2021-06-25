277ecadb3017ab6b859e1ea6001378c00c7ecda8
Cat Hanbit <72328552+LeeSeunghyeon-1@users.noreply.github.com>
2021-06-26 02:38:41 +0900

    .

---
+++ Week01/Day06/bj2525_lsh.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const time = input().split(" ");
+const sub = parseInt(input());
+
+let h = parseInt(time[0]);
+let min = parseInt(time[1]);
+
+min += sub;
+
+while (min >= 60) {
+  min -= 60;
+  h++;
+}
+
+while (h >= 24) {
+  h -= 24;
+}
+
+console.log(h + " " + min);
