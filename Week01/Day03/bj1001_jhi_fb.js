32d1c8a9d8da1f36f64381f575152b99c846f889
ailraon <aadsf1789@naver.com>
2021-06-25 15:55:06 +0900

    Day03 피드백 반영 완료

---
+++ Week01/Day03/bj1001_jhi_fb.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const numbers = input().split(" ");
+console.log(numbers[0] - numbers[1]);
