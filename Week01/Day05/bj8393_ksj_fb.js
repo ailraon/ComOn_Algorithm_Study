2def9c82a4fef65ae80cac9a9367de2f4af01676
pocachp <pocachip0318@gmail.com>
2021-06-28 03:35:58 +0900

    8일차 Day05 피드백 반영

---
+++ Week01/Day05/bj8393_ksj_fb.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+////읽기 쉬워서 좋았습니다. :22 :333
+
+let num = input();
+let sum = 0;
+
+for (let i = 0; i <= num; i++) {
+  sum += i;
+}
+console.log(sum);
