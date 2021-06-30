2571350b6352a430fcf677dd663a05db9aa8e34b
ailraon <aadsf1789@naver.com>
2021-06-30 15:49:43 +0900

    Day10 문제풀이 완료

---
+++ Week02/Day10/bj5554_jhi.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let totalSecond = 0;
+
+for(let i = 0; i < 4; i++) {
+    totalSecond += parseInt(input());
+}
+
+let time = {
+    minute : parseInt(totalSecond / 60),
+    second : totalSecond % 60
+};
+
+console.log(time.minute);
+console.log(time.second);
