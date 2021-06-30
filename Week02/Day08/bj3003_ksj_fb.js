e192c5c9006d7b6592ebea39f43adc5bab13a7d2
pocachp <pocachip0318@gmail.com>
2021-06-30 14:23:39 +0900

    Day08 피드백 반영 파일에 참고할 내용 추가

--- Week02/Day08/bj3003_ksj_fb.js
+++ Week02/Day08/bj3003_ksj_fb.js
@@ -9,10 +9,13 @@ let blackPieces = [1, 1, 2, 2, 2, 8];
 let whitePieces = input()
     .split(" ")
     .map((whitePieces) => Number(whitePieces));
+    //이렇게 한줄씩 따로 쓸 수 있는 점 알게됨
 
 let fullSet = [];
 for(let i=0; i<6; i++){
-    fullSet[i] = blackPieces[i] - whitePieces[i];
+    fullSet[i] = blackPieces[i] - whitePieces[i]; 
+    // fullSet.push(blackPieces[i] - whitePieces[i]); 도 가능
+    // push: 배열 끝에 하나 이상 요소 추가하고, 배열의 새로운 길이 반환
 }
 
 console.log(fullSet.join(" ")); // join : 배열의 모든 요소를 연결해 하나의 문자열로 만듬
