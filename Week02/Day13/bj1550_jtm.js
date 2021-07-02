0e7d9bf65882d698c880e7889a125792daab5542
Taek-min <jtm11245@gmail.com>
2021-07-03 00:37:17 +0900

    no message

---
+++ Week02/Day13/bj1550_jtm.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+var number =  input();
+console.log(parseInt(number, 16));
