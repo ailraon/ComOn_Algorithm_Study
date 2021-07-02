e1e86ca83f338efa87bcc169328e87168d67d089
Cat Hanbit <72328552+LeeSeunghyeon-1@users.noreply.github.com>
2021-07-03 01:49:57 +0900

    .

---
+++ Week02/Day11/bj20492_lsh_fb.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const tax = parseInt(input());
+
+console.log(tax * 0.78 + " " + tax * 0.956);
+
+//// 깔끔합니다 :22
---
+++ Week02/Day13/bj1550_lsh.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let hex = input();
+console.log(parseInt(hex, 16));
