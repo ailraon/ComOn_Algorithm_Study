c5153db01782dbb2e76c5568318138823e98ed09
Cat Hanbit <72328552+LeeSeunghyeon-1@users.noreply.github.com>
2021-07-02 14:01:18 +0900

    .

---
+++ Week02/Day10/bj5554_lsh_fb.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let time = 0;
+let sum = 0;
+
+for (let i = 0; i < 4; i++) {
+  time = parseInt(input());
+  sum += time;
+}
+
+console.log(parseInt(sum / 60));
+console.log(parseInt(sum % 60));
