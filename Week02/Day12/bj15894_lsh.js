d53f40372f458bd4553bcf553567e0194b16d22e
Cat Hanbit <72328552+LeeSeunghyeon-1@users.noreply.github.com>
2021-07-02 22:59:08 +0900

    Update bj15894_lsh.js

---
+++ Week02/Day12/bj15894_lsh.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let square = parseInt(input());
+console.log(square * 4);
