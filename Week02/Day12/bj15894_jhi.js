381752b8cc7966ddaf5a04cc5604d2ec84f6a419
ailraon <aadsf1789@naver.com>
2021-07-03 13:59:56 +0900

    Day12 문제 풀이 완료

---
+++ Week02/Day12/bj15894_jhi.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const num = input();
+
+console.log(Number(num * 4));
