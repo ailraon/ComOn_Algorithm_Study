e9c58712a149c95f84759ababf014d74f233fd6f
pocachp <pocachip0318@gmail.com>
2021-07-03 15:14:48 +0900

    13일차 Day11 피드백 반영

---
+++ Week02/Day11/bj20492_ksj_fb.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const tax = parseInt(input());
+
+let first = tax * (1-0.22);
+let second = tax * 0.8 + ((tax*0.2*(1-0.22)));
+
+//// 팁: 템플릿 리터럴을 사용하면 console.log(`${first}} ${second}`); 요렇게 쓸 수도 있습니다
+//// 요긴하게 쓰일 수 있을 거 같아요!
+console.log(`${first} ${second}`);
+//// 잘했습니다!
