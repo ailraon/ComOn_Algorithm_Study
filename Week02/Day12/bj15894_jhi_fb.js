9999d63ce738583936da94706d7c7c31c62a7aa3
ailraon <aadsf1789@naver.com>
2021-07-05 00:06:56 +0900

    Day12 피드백 반영 완료

---
+++ Week02/Day12/bj15894_jhi_fb.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const num = input();
+
+console.log(Number(num * 4));
+
+////잘했습니다 :22 :33
