baabb6e8230a6473dae1120868383a154c1a3cd7
ailraon <aadsf1789@naver.com>
2021-07-05 00:05:00 +0900

    Day15 문제풀이 완료

---
+++ Week03/Day15/bj2420_jhi.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let number = input().split(" ");
+
+console.log(Math.abs(number[0] - number[1]));
