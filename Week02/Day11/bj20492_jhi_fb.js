983da11bc2e61ad735fae826f2f4f173dbcee1dd
ailraon <aadsf1789@naver.com>
2021-07-04 14:17:09 +0900

    Day11 피드백 반영 완료

---
+++ Week02/Day11/bj20492_jhi_fb.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let reward = input();
+let result = (reward-reward*0.22) + ' ' + (reward-reward*0.2*0.22);
+console.log(result);
+
+//// 깔끔합니다
+//// 잘했네요 : 22
