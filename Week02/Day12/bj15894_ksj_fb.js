f80629abc72a704ce7b9f58dd8a821b2b0d40096
pocachp <pocachip0318@gmail.com>
2021-07-05 16:01:24 +0900

    15일차 피드백 반영

---
+++ Week02/Day12/bj15894_ksj_fb.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let side = parseInt(input());
+let result = side * 4;
+
+console.log(result);
