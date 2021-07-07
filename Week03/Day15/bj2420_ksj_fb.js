120bb52dc12961303ae58cca54a1db806ae7f2e1
pocachp <pocachip0318@gmail.com>
2021-07-07 11:29:00 +0900

    17일차 피드백 반영

---
+++ Week03/Day15/bj2420_ksj_fb.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let domain = input().split(" ");
+let firstDomain = parseInt(domain[0]);
+let secondDomain = parseInt(domain[1]);
+let result = Math.abs(domain[0] - domain[1]);
+
+console.log(result);
+
+
+//// 백준에 제출했을때 '틀렸습니다'로 나옵니다. 꼭 백준에 먼저 제출해본 뒤 올려주세요! 
+//// 백준에서 맞았던 코드말고 다른 걸 올렸었네요! 피드백 반영했습니다
