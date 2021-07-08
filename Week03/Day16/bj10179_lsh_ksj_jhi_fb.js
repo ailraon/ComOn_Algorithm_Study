63973eba454b2144b5e89520f412619f5c1e3374
pocachp <pocachip0318@gmail.com>
2021-07-08 19:46:37 +0900

    18일차 페프 피드백 반영

---
+++ Week03/Day16/bj10179_lsh_ksj_jhi_fb.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let len = parseInt(input());
+
+for (let i = 0; i < len; i++) {
+  let num = parseFloat(input());
+  console.log("$" + (num * 0.8).toFixed(2));
+}
