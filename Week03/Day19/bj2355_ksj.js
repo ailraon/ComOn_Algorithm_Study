c8c6f8501e568c13e300212f405f9184a521ec2e
pocachp <pocachip0318@gmail.com>
2021-07-09 17:42:38 +0900

    19일차 문제 풀이

---
+++ Week03/Day19/bj2355_ksj.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let num = input().split(" ");
+let A = Number(num[0]);
+let B = Number(num[1]);
+let max = 0;
+let min = 0;
+
+if(A > B){
+    max = A;
+    min = B;
+}
+
+else{
+    max = B;
+    min = A;
+}
+
+let count = max - min + 1;
+let result = (max + min) * (Math.floor(count/2));
+let mid = (max + min)/2;
+
+if(count% 2 === 0){
+    console.log(result);
+}
+
+else {
+	console.log(result + mid);
+}
