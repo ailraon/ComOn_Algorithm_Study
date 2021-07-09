9f293f1dc682aa286b67ff545fe6c663570f57d3
ailraon <aadsf1789@naver.com>
2021-07-09 15:10:10 +0900

    Day17 피드백 반영

---
+++ Week03/Day17/bj5893_jhi_fb.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let bin = "0b" + input();
+console.log((BigInt(bin) * 17n).toString(2));
+
+//// 깔끔합니다. 문제 조건을 잘 파악하고 BigInt 활용도 잘해주신 것 같습니다 : 22
+//// BigInt에 대해서 알게 되었습니다
