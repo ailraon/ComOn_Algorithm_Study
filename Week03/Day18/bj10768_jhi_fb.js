367a29dbb9a5f58965d2846eff8d051530954f35
ailraon <aadsf1789@naver.com>
2021-07-11 02:04:30 +0900

    Day18 피드백 반영

---
+++ Week03/Day18/bj10768_jhi_fb.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let month = input();
+let day = input();
+
+console.log(
+  Number(month) > 2
+    ? "After"
+    : Number(month) < 2
+    ? "Before"
+    : Number(day) > 18
+    ? "After"
+    : Number(day) < 18
+    ? "Before"
+    : "Special"
+);
+
+//// if문 대신 삼항연산으로 표현한 점이 신선했습니다 : 22 : 333
