f2529f3f2d73e80f638d72fd5a05454318087bf1
김진홍 <deepred.kim@gmail.com>
2021-07-12 21:56:36 +0900

    20일차 피드백

--- Week03/Day20/bj10871_jhi.js
+++ Week03/Day20/bj10871_jhi.js
@@ -6,6 +6,8 @@ const input = (() => {
 })();
 
 let condition = input().split(" ");
+//// condition(상태, 조건)이라는 변수명으로 이게 어떤 변수인지 알기 어려웠어요. 참/거짓 값인가? 했는데 문제 조건을 말한거였네요
+//// [N, X]로 받으면 더 깔끔하고 명확할 것 같습니다! map으로 parseInt도 다 해주면 Number()도 해주지 않아도 되기도 하고요.
 
 let numbers = input().split(" ");
 let result = "";
--- Week03/Day20/bj10871_ksj.js
+++ Week03/Day20/bj10871_ksj.js
@@ -7,6 +7,7 @@ const input = (() => {
 
 //// map을 활용한 점은 좋았으나, NandX 변수에 담지 말고 [N, X] 로 한번에 입력받았으면 더 좋았을 것 같습니다.
 let NandX = input().split(" ").map(NandX => Number(NandX));
+//// map을 활용한 점은 좋았으나, NandX 변수에 담지 말고 [N, X] 로 한번에 입력받았으면 더 좋았을 것 같습니다. :22
 let N = NandX[0];
 let X = NandX[1];
 let num = input().split(" ").map(Number);
