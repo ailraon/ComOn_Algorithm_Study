661b6075ffd83a2055f23f057764241f7ec87d40
ailraon <aadsf1789@naver.com>
2021-07-13 19:03:43 +0900

    Day23 페어프로그래밍 문제풀이

---
+++ Week04/Day23/bj4880_jhi_ksj_lsh.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+for(let i = 0; ; i++) {
+  let [a1, a2, a3] = input().split(" ").map(num => parseInt(num));
+  let result = "";
+
+  if(a1 === 0 && a2 === 0 && a3 === 0) break;
+
+  if(a2 - a1 === a3 - a2) { // 참이면 등차 거짓이면 등비
+    result += "AP " + (a3 + a2 - a1);
+  } else {
+    result += "GP " + (a3 * a2 / a1);
+  }
+  console.log(result);
+}
