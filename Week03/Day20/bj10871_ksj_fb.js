3babcee68eb7cf5b235e0313d52046cc8a66f8c4
pocachp <pocachip0318@gmail.com>
2021-07-14 01:56:25 +0900

    23일차 피드백 반영

---
+++ Week03/Day20/bj10871_ksj_fb.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+//// map을 활용한 점은 좋았으나, NandX 변수에 담지 말고 [N, X] 로 한번에 입력받았으면 더 좋았을 것 같습니다. :22
+let [N, X]= input()
+  .split(" ")
+  .map((NandX) => Number(NandX));
+let num = input().split(" ").map(Number);
+let arr = [];
+
+for (let i = 0; i < N; i++) {
+  if (num[i] < X) {
+    arr += num[i] + " ";
+  }
+}
+
+console.log(arr);
+//// map을 사용한 점 좋았습니다.
