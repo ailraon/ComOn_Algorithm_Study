a2dda301481ba96e798f5126b853c1a66a585239
김진홍 <deepred.kim@gmail.com>
2021-07-14 21:48:21 +0900

    23일차 피드백

--- Week04/Day23/bj4880_jhi_ksj_lsh.js
+++ Week04/Day23/bj4880_jhi_ksj_lsh.js
@@ -1,20 +1,24 @@
-const fs = require("fs");
-const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const fs = require('fs');
+const stdin = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const input = (() => {
   let line = 0;
   return () => stdin[line++];
 })();
 
-for(let i = 0; ; i++) {
-  let [a1, a2, a3] = input().split(" ").map(num => parseInt(num));
-  let result = "";
+//// while문으로 풀었다면 더 깔끔했을 것 같습니다
+for (let i = 0; ; i++) {
+  let [a1, a2, a3] = input()
+    .split(' ')
+    .map((num) => parseInt(num));
+  let result = '';
 
-  if(a1 === 0 && a2 === 0 && a3 === 0) break;
+  if (a1 === 0 && a2 === 0 && a3 === 0) break;
 
-  if(a2 - a1 === a3 - a2) { // 참이면 등차 거짓이면 등비
-    result += "AP " + (a3 + a2 - a1);
+  if (a2 - a1 === a3 - a2) {
+    // 참이면 등차 거짓이면 등비
+    result += 'AP ' + (a3 + a2 - a1);
   } else {
-    result += "GP " + (a3 * a2 / a1);
+    result += 'GP ' + (a3 * a2) / a1;
   }
   console.log(result);
 }
