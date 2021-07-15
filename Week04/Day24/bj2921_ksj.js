d46e55269ce6fe18a1d6ff191166b025b81495f2
Deepred <deepred.kim@gmail.com>
2021-07-15 10:08:46 +0900

    Update bj2921_ksj.js

--- Week04/Day24/bj2921_ksj.js
+++ Week04/Day24/bj2921_ksj.js
@@ -7,6 +7,7 @@ const input = (() => {
 
 let N = parseInt(input());
 //// 도미노 묶음의 개수라는 의미로 dominoBundles 등으로 짓는건 어떨까요?
+//// dominoBundles로 그렇게 명확한 의미의 변수명은 아니긴 하지만, 작성하는 코드의 의도를 이렇게 변수명, 함수명으로 표현하는 연습을 평소 하시면 좋을 것 같습니다!
 let sum = 0;
 
 for(let i=1; i<=N; i++){
