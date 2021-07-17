6e8617eaa69ed7715544a648db0fb62c21340576
김진홍 <deepred.kim@gmail.com>
2021-07-17 14:51:09 +0900

    26일차 피드백

--- Week04/Day26/bj2720_jhi.js
+++ Week04/Day26/bj2720_jhi.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const fs = require('fs');
+const stdin = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const input = (() => {
   let line = 0;
   return () => stdin[line++];
@@ -13,19 +13,22 @@ const Penny = 1;
 let change = 0;
 let times = Number(input());
 
-for(let i = 0; i < times; i++) {
-    change = Number(input());
-    changeMoney(change);
+for (let i = 0; i < times; i++) {
+  change = Number(input());
+  changeMoney(change);
 }
 
+//// 함수로 분리해본 점 굳굳
+//// 다만 함수는 동사로 시작하는게 좋습니다! printChange 또는 printChangeMoney로 했다면 더 좋았을 것 같아요
 function changeMoney(change) {
-    let divisionQuarter = parseInt(change / Quarter);
-    let divisionChange = change % Quarter;
-    let divisionDime = parseInt(divisionChange / Dime);
-    divisionChange = divisionChange % Dime;
-    let divisionNickel = parseInt(divisionChange / Nickel);
-    divisionChange = divisionChange % Nickel;
-    let divisionPenny = parseInt(divisionChange / Penny);
-    console.log(`${divisionQuarter} ${divisionDime} ${divisionNickel} ${divisionPenny}`);
+  let divisionQuarter = parseInt(change / Quarter);
+  let divisionChange = change % Quarter;
+  let divisionDime = parseInt(divisionChange / Dime);
+  divisionChange = divisionChange % Dime;
+  let divisionNickel = parseInt(divisionChange / Nickel);
+  divisionChange = divisionChange % Nickel;
+  let divisionPenny = parseInt(divisionChange / Penny);
+  console.log(
+    `${divisionQuarter} ${divisionDime} ${divisionNickel} ${divisionPenny}`
+  );
 }
-
--- Week04/Day26/bj2720_ksj.js
+++ Week04/Day26/bj2720_ksj.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const fs = require('fs');
+const stdin = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const input = (() => {
   let line = 0;
   return () => stdin[line++];
@@ -7,13 +7,15 @@ const input = (() => {
 
 let cnt = parseInt(input());
 
-for(let i=0; i<cnt; i++){
-    let change = parseInt(input());
-    
-    let quarter = Math.floor(change/25);
-    let dime = Math.floor((change%25)/10);
-    let nickel = Math.floor((change%25%10)/5);
-    let penny = Math.floor(change%25%10%5);
-    
-	console.log(`${quarter} ${dime} ${nickel} ${penny}`);
+for (let i = 0; i < cnt; i++) {
+  let change = parseInt(input());
+
+  let quarter = Math.floor(change / 25);
+  let dime = Math.floor((change % 25) / 10);
+  let nickel = Math.floor(((change % 25) % 10) / 5);
+  let penny = Math.floor(((change % 25) % 10) % 5);
+
+  console.log(`${quarter} ${dime} ${nickel} ${penny}`);
 }
+
+//// 깔끔합니다
