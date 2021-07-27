b83893d964f20a4b390aa8eee45d0e080028035c
ailraon <aadsf1789@naver.com>
2021-07-27 21:16:21 +0900

    Day27 피드백 반영 완료

---
+++ Week04/Day27/bj15700_jhi_fb.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const stdin = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+//// 첫 글자는 소문자로 시작하는게 관례입니다
+const [wallWidth, wallHeight] = input()
+  .split(' ')
+  .map((num) => BigInt(num));
+
+let result = BigInt((wallHeight * wallWidth) / 2n);
+console.log(result.toString());
+
+//// 깔끔합니다 :22
