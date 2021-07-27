95e4e233355f101b445044640ddbb2b7c95f0ab4
ailraon <aadsf1789@naver.com>
2021-07-27 21:10:34 +0900

    Day29 문제풀이

---
+++ Week06/Day29/bj4153_jhi.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+
+while(true) { 
+    let [rt1, rt2, rt3] = input().split(' ').map((num) => parseInt(num));
+    if(rt1 === 0 && rt2 === 0 && rt3 === 0) {
+        break;
+    }
+    if((rt1 * rt1 === (rt2 * rt2 + rt3 * rt3) ||
+     rt2 * rt2 === (rt1 * rt1 + rt3 * rt3) ||
+      rt3 * rt3 === (rt2 * rt2 + rt1 * rt1))) {
+        console.log("right");
+    } else {
+        console.log("wrong");
+    }
+}
