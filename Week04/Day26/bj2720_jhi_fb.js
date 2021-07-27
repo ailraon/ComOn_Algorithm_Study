7841081495b399532aff37cab210f1671131d395
ailraon <aadsf1789@naver.com>
2021-07-27 21:16:09 +0900

    Day26 피드백 반영

---
+++ Week04/Day26/bj2720_jhi_fb.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const stdin = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const Quarter = 25;
+const Dime = 10;
+const Nickel = 5;
+const Penny = 1;
+
+let change = 0;
+let times = Number(input());
+
+for (let i = 0; i < times; i++) {
+  change = Number(input());
+  changeMoney(change);
+}
+
+function printChangeMoney(change) {
+  let divisionQuarter = parseInt(change / Quarter);
+  let divisionChange = change % Quarter;
+  let divisionDime = parseInt(divisionChange / Dime);
+  divisionChange = divisionChange % Dime;
+  let divisionNickel = parseInt(divisionChange / Nickel);
+  divisionChange = divisionChange % Nickel;
+  let divisionPenny = parseInt(divisionChange / Penny);
+  console.log(
+    `${divisionQuarter} ${divisionDime} ${divisionNickel} ${divisionPenny}`
+  );
+}
