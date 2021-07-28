4cbbb4b80f685f5ffda63fd59645951672ab29cb
Deepred <deepred.kim@gmail.com>
2021-07-28 16:06:47 +0900

    Update bj2443_ksj_jhi.js

--- Week06/Day30/bj2443_ksj_jhi.js
+++ Week06/Day30/bj2443_ksj_jhi.js
@@ -7,6 +7,20 @@ const input = (() => {
 
 let len = Number(input());
 
+//// console.log / process.stdout.write / System.out.print print 등등 이런 출력함수는
+//// 실행 시간을 단축하려면 한번만 쓰는게 좋아
+//// 왜냐면 한번 호출할 때마다 시간이 꽤 걸리거든
+
+//// 어떻게 한번만 쓰냐?
+//// let result = []; 빈 배열을 하나 두고
+//// 결과값을 result.push(" ") 푸쉬해
+//// 그리고 마지막에 console.log(result.join("")) 하면 push 했던 값들이 전부 붙어
+
+//// result = "" 빈 문자열 담아놓고
+//// result += "*" 이렇게 붙이는것도 나쁘진 않은데
+//// 이 방법보다는 배열에 담는게 시간효율이 더 좋대
+
+//// 앞으로는 배열에 담아서 한번씩만 출력하길 바람!
 for(let i = 0; i<len; i++){
     for(let j=0; j<2*len-(i+1); j++) {
         if(j < i){
