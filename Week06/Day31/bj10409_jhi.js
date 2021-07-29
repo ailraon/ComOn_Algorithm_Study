2bc21c354b5cde287d56245bbd706ea4b7b7eaff
Deepred <deepred.kim@gmail.com>
2021-07-29 11:34:36 +0900

    31일차 피드백

--- Week06/Day31/bj10409_jhi.js
+++ Week06/Day31/bj10409_jhi.js
@@ -7,8 +7,10 @@ const input = (() => {
 
 let [setCount, setTime] = input().split(' ').map((num) => parseInt(num));
 
+//// works 등으로 더 의미를 명확하게 하면 좋을 것 같아
 let count = 0;
 let sumTime = 0;
+//// 변수/함수명 첫 글자는 소문자가 관례~
 let Times = input().split(' ');
 for(let i = 0; i < setCount; i++) {
     sumTime += Number(Times[i]);
--- Week06/Day31/bj1065_jhi.js
+++ Week06/Day31/bj1065_jhi.js
@@ -5,6 +5,11 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+//// 주석은 '변수명, 함수명으로도 도저히 설명이 불가능할때'만!
+//// 코드를 설명하는 방법은 최대한 아래 방법만 사용해줘
+  //// 1. 상수, 변수명 (오직 설명하기 위한 용도로도 변수를 만들어도 돼)
+  //// 2. 함수명 (엄청 짧은 함수여도 상관없어)
+  //// 3. 줄바꿈 (맥락이 다를때 줄바꿈 두 번을 쳐서 맥락이 달라졌음을 표현)
 let N = Number(input()); // 입력받는 수
 
 let hanN = 0; // 한수의 갯수
@@ -15,6 +20,10 @@ if(N < 100) {
 } else {
     hanN = 99;
     for(let i = 100; i <= N; i++) {
+        //// 자리수를 분리하는 것도 함수에 맡기는건 어때?
+        //// isHansoo: 정수를 받아 한수라면 true 아니라면 false를 반환하는 함수 뭐 이런식?
+
+        //// 참고로 '어때?' 묻는건 항상 제안하는 의미로 쓰고있어
         let oneDigit = i%10;
         let tenDigit = parseInt((i/10)%10);
         let hundredDigit = parseInt(i/100);
@@ -26,7 +35,9 @@ if(N < 100) {
 }
 
 
+//// 일관성있게 oneDigit, tenDigit, hundredDigit 했으면 더 좋았을 것 같아
 function isArithmeticSequence(one, two, three) {
+    //// if문을 쓰지 않고 조건문으로 쓴 걸 그대로 반환해도 돼
     if((one - two) === (two - three)) {
         return true;
     } else {
