78389dd399886673d71037d06d8a3016d1e35bb2
Deepred <deepred.kim@gmail.com>
2021-07-30 13:15:07 +0900

    32일차 피드백

--- Week06/Day32/bj9536_jhi.js
+++ Week06/Day32/bj9536_jhi.js
@@ -7,22 +7,26 @@ const input = (() => {
 
 let testCase = Number(input());
 
-for(let i = 0; i < testCase; i++) {
-    let recoding = input().split(' ');
-    let result = '';
-    while(true) {
-      let animals = input();
-      if(animals === "what does the fox say?") break;
-      animals = animals.split(' ');
-      for(let j = 0; j < recoding.length; j++) {
-        if(animals[2] === recoding[j]) {
-          recoding[j] = '';
-        }
+//// 직관적인 변수명, 깔끔한 로직 굳굳
+for (let i = 0; i < testCase; i++) {
+  let recoding = input().split(' ');
+  let result = '';
+  while (true) {
+    //// 울음소리니까 animals보단 cryings가 맞을듯!
+    let animals = input();
+    if (animals === 'what does the fox say?') break;
+    animals = animals.split(' ');
+    for (let j = 0; j < recoding.length; j++) {
+      if (animals[2] === recoding[j]) {
+        //// 비우는것보단 지우는 게 더 깔끔할 것 같아
+        recoding[j] = '';
       }
     }
-    for(let j = 0; j < recoding.length; j++) {
-      if(recoding[j] !== '') 
-        result += (recoding[j] + ' ');
-    }
-    console.log(result);
+  }
+
+  //// recoding.join(' ') 하면 27~30줄이 돼
+  for (let j = 0; j < recoding.length; j++) {
+    if (recoding[j] !== '') result += recoding[j] + ' ';
+  }
+  console.log(result);
 }
