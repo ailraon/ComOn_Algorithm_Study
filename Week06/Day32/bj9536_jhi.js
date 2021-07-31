e8661ac735852309f10baca42cb6075630f42685
김진홍 <deepred.kim@gmail.com>
2021-08-01 00:50:57 +0900

    Update bj9536_jhi.js

--- Week06/Day32/bj9536_jhi.js
+++ Week06/Day32/bj9536_jhi.js
@@ -10,9 +10,6 @@ let testCase = Number(input());
 //// 직관적인 변수명, 깔끔한 로직 굳굳
 for (let i = 0; i < testCase; i++) {
   let recoding = input().split(' ');
-  //// 이 result는 for 블록 안에서만 존재하고
-  //// for을 벗어나는 순간 할당해제가 돼서 없는 변수가 돼 (let은 블록 스코프)
-  //// 16줄의 result와 33줄의 result는 서로 다른 변수임
   let result = '';
   while (true) {
     //// 'cow goes moo'를 'animals'로 표현하는건 좀 적절하지 않은 것 같아
