fded0765d09c370e078c66200f3a85fafa599caa
ailraon <aadsf1789@naver.com>
2021-08-02 19:04:34 +0900

    Day36 문제풀이

---
+++ Week07/Day36/bj4673_jhi.js
@@ -0,0 +1,30 @@
+let allNumber = new Array(10000);
+for(let i = 0; i < allNumber.length; i++) {
+  allNumber[i] = i+1;
+}
+let sequenceNumber = Array.from(allNumber, x => makeSequence(x));
+let selfNumber = '';
+
+for(let i = 0; i < 10000; i++) {
+  if(!(inNumber(Number(allNumber[i]), sequenceNumber))) {
+    selfNumber += String(allNumber[i]) + '\n';
+  }
+}
+
+console.log(selfNumber);
+
+function makeSequence (num) {
+  let result = Number(num);
+  let digit = String(num);
+  for(let i = 0; i < digit.length; i++) {
+    result += Number(digit.charAt(i));
+  }
+  return result;
+}
+
+function inNumber (num, sequenceNumber) {
+  for(let i = parseInt(num/2)-1; i <= num; i++) {
+    if(num === sequenceNumber[i]) return true;
+  }
+  return false;
+}
