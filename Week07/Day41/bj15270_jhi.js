87f04b508bb38d83efa32dcdf06abfd0c921f700
Deepred <deepred.kim@gmail.com>
2021-08-10 09:32:02 +0900

    41일차 피드백

--- Week07/Day41/bj15270_jhi.js
+++ Week07/Day41/bj15270_jhi.js
@@ -5,9 +5,11 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+//// 짧게해도 충분히 의미가 전달될 것 같다 싶으면 Relations 정도로 표현해도 됨!
 let [numberOfStudentsInClass, numberOfRelationships] = input().split(' ').map((num) => parseInt(num));
 let isClassPaired = new Array(numberOfStudentsInClass+1);
 
+//// new Array(~) 안하고 그냥 [] 해도 됨. 길이 지정할 필요 없음
 let classRelationships = new Array(numberOfStudentsInClass+1);
 
 for (let i = 0; i < numberOfRelationships; i++) {
@@ -23,6 +25,8 @@ let numberOfPairOnStage = countMaxPair(1, 0);
 
 console.log(numberOfPairOnStage < numberOfStudentsInClass ? numberOfPairOnStage + 1 : numberOfStudentsInClass);
 
+//// 이 완전탐색(브루트포스) 아이디어 잘 기억해두기 바람
+//// 재귀 문제에서 많이 나오는 풀이임
 function countMaxPair (checkAttendenceNumber, pairedNumber) {
 	if (checkAttendenceNumber > numberOfStudentsInClass) return pairedNumber;
 
