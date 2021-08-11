d3392c90bbdc2605c73777f8472e66c56392b0a9
김진홍 <deepred.kim@gmail.com>
2021-08-11 20:04:15 +0900

    44일차 피드백

--- Week08/Day44/bj18511_kjhjhi.js
+++ Week08/Day44/bj18511_kjhjhi.js
@@ -11,6 +11,8 @@ let DIGIT_LIMIT = N.length;
 
 let usableNumbers = input().split(' ');
 
+//// 이렇게 어려웠던 문제일수록 다른 사람 코드를 참고하는 게 많은 도움이 됨
+//// 랭킹에서 다른 사람 코드들을 보고 그 중 제일 인상깊었던 소스코드 주소랑 짧막한 감상평 남기기 바람
 const getMaxNumInLimit = (numbersToAdd, strNumber) => {
   if (numbersToAdd == 0) return parseInt(strNumber);
   
