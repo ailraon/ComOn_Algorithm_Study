28fa3f96c552d44522c8744ceb321c8ce5510f25
Deepred <deepred.kim@gmail.com>
2021-08-13 14:19:45 +0900

    46일차 피드백

--- Week08/Day46/bj4779_jhi.js
+++ Week08/Day46/bj4779_jhi.js
@@ -8,6 +8,7 @@ const input = (() => {
 const inputCount = stdin.length;
 let result = '';
 
+//// (inputCount-- > 0) 하면 j 안써도 됨
 let j = 0;
 while (j < inputCount) {
   let threeToTheNPower = Math.pow(3, Number(input()));
@@ -18,6 +19,8 @@ while (j < inputCount) {
 
 console.log(result);
 
+//// indexNum?은 두루뭉실한듯
+//// '세 덩어리 중 몇번째인지'가 indexNum인거고, '그게 1이면 세 덩어리 중 가운데라서' 공백을 채워준다는 의도를 한번 최대한 표현해보려 하기 바람
 function cantorSet (stringLength, indexNum) {
 	if (indexNum === 1) return " ".repeat(stringLength);
   if (stringLength === 1) return "-";
