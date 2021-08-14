43feb6a89aab8d3c42050c2e1f262de04befd2d6
ailraon <aadsf1789@naver.com>
2021-08-15 00:51:27 +0900

    Day46 피드백 반영

---
+++ Week08/Day46/bj4779_jhi_fb.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const inputCount = stdin.length;
+let result = '';
+
+while (inputCount-- > 0) {
+  let threeToTheNPower = Math.pow(3, Number(input()));
+
+  result += cantorSet(threeToTheNPower, 0) + '\n';
+}
+
+console.log(result);
+
+function cantorSet (stringLength, isMiddleIndex) {
+	if (isMiddleIndex === 1) return " ".repeat(stringLength);
+  if (stringLength === 1) return "-";
+	let cantorSets = '';
+	for(let i = 0; i < 3; i++) {
+		cantorSets += cantorSet(stringLength / 3, i);
+	}
+	return String(cantorSets);
+}
