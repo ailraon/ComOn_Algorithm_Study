837744862f599db1491741c8fa71de8c53c529e5
ailraon <aadsf1789@naver.com>
2021-08-16 21:28:36 +0900

    Day50 문제풀이

---
+++ Week09/Day50/bj6012_jhi.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let numberOfCows = Number(input());
+let result = 0;
+oddCowOutDance(1, numberOfCows);
+console.log(result);
+
+function oddCowOutDance (startNumber, endNumber) {
+	let difference = endNumber - startNumber;
+	if (difference > 1) {
+		let middleNumber = ((endNumber + startNumber) % 2 === 0) ? (endNumber + startNumber)  / 2 : (endNumber + startNumber - 1) / 2;
+		oddCowOutDance(startNumber, middleNumber);
+		oddCowOutDance(middleNumber + 1, endNumber);
+	} else {
+		if (difference === 0) {
+			return 0;
+		} 
+		if (difference === 1) {
+			return result += startNumber * endNumber;
+		}
+	}
+}
