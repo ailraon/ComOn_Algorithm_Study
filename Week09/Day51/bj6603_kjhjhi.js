36e44d55ffc386ebc819c570a2f76f2a0e7a29f2
김진홍 <deepred.kim@gmail.com>
2021-08-18 20:49:04 +0900

    51일차

---
+++ Week09/Day51/bj6603_kjhjhi.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let lottoCombinations;
+const makeLottoCombinations = (amountToPick, elementsToPick, lotto) => {
+  if (amountToPick === 0) {
+    lottoCombinations.push(lotto);
+    return;
+  }
+
+  let pickEndIdx = elementsToPick.length - amountToPick;
+  for (let i = 0; i <= pickEndIdx; i++) {
+    makeLottoCombinations(amountToPick - 1,
+      elementsToPick.slice(i + 1),
+      `${lotto}${elementsToPick[i]} `
+    );
+  }
+}
+
+let line;
+while ((line = input()) !== "0") {
+  const [elementSize, ...elementsToPick] = line.split(" ").map(Number);
+
+  lottoCombinations = [];
+  makeLottoCombinations(6, elementsToPick, "");
+
+  console.log(lottoCombinations.join("\n") + "\n");
+}
