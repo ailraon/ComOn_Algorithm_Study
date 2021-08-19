b3e8447f6921173ebca441e81da282b2211d9131
ailraon <aadsf1789@naver.com>
2021-08-19 18:56:09 +0900

    Week8 피드백 반영

---
+++ Week08/Day48/bj9742_jhi_fb.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const stdin = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let inputCount = stdin.length;
+let result = '';
+let i = 0
+while (i < inputCount) {
+  let inputData = input().split(' ');
+  result += inputData[0] + ' ' + inputData[1] + ' = ' + makePermutation(inputData[0], Number(inputData[1])) + '\n';
+  i++;
+}
+console.log(result);
+
+function makePermutation (str, permutationNum) {
+  if (str.length === 1) return str;
+
+  let permutationstr = '';
+  let pattern = factorial(str.length - 1);
+  for (let i = 1; i < str.length + 1; i++) {
+    if (pattern*i >= permutationNum) {
+      i--;
+      permutationstr = str[i] + makePermutation(str.replace(str[i], ''), permutationNum - pattern * i);
+
+      return permutationstr;
+    }
+  }
+  return 'No permutation';
+}
+
+function factorial(num) {
+  let result = 1;
+  for ( let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
+}
