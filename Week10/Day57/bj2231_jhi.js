788229ce48db42d43021cd8bbda5c7cd2ded0d7d
Deepred <deepred.kim@gmail.com>
2021-08-24 14:33:58 +0900

    57일차 피드백

--- Week10/Day57/bj2231_jhi.js
+++ Week10/Day57/bj2231_jhi.js
@@ -5,10 +5,13 @@ const input = (() => {
   return () => stdin[line++];
 })();
 
+//// 이름 지을꺼리 없고+입력 변수가 한개뿐이면
+//// 그냥 문제에서 주어진대로 N 으로 지어버리는게 제일임
 const number = Number(input());
 
 let result = 0;
 
+//// for( 말고 for ( 이렇게 하는게 관례
 for(let i = 0; i < number; i++) {
 	let naturalNumber = sumDecomposition(i);
 	if (naturalNumber === number) {
@@ -22,6 +25,7 @@ console.log(result);
 function sumDecomposition (number) {
 	let numberStr = String(number);
 	let result = number;
+	//// for( 말고 for ( 이렇게 하는게 관례
 	for(let i = 0; i < numberStr.length; i++) {
 		result += Number(numberStr[i]);
 	}
